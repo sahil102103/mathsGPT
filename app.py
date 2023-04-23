d9535a06e12f3eb98eae9b78767aa07e727ac1fd
sahil102103 <sahilshah102103@gmail.com>
2023-04-22 21:25:18 -0400

    first push

--- app.py
+++ app.py
@@ -1,35 +1,61 @@
 import os
-
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+@app.route("/clear_history", methods=["POST"])
+def clear_history():
+    session.pop("history", None)
+    return redirect(url_for("index"))
 
-@app.route("/", methods=("GET", "POST"))
+@app.route("/request_help", methods=["POST"])
+def request_help():
+    # Get the previous math problem from session history
+    math_problem, _ = session["history"][-1]
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=generate_prompt(math_problem),
+        temperature=0.6,
+    )
+    result = response.choices[0].text.split("\n")
+    # Add to session history
+    session.setdefault("history", []).append((math_problem, result))
+    return redirect(url_for("index"))
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        math_problem = request.form["math_problem"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(math_problem),
             temperature=0.6,
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        result = response.choices[0].text.split("\n")
+        # Add to session history
+        session.setdefault("history", []).append((math_problem, result))
+        return render_template("index.html", result=result)
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+    return render_template("index.html")
 
+def generate_prompt(math_problem):
+    return f"""The following is a conversation with an AI assistant. The assistant is meant to guide you to solve the problem using the Socratic method.
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+Assistant: Can you tell me more about {math_problem}?
+Human: It's a problem involving...
+Assistant: What do you know about {math_problem} so far?
+Human: I know that...
+Assistant: What steps have you taken to solve the problem?
+Human: I have tried...
+Assistant: Have you considered...
+Human: No, I haven't.
+Assistant: How can you break down {math_problem} into smaller parts?
+Human: I can break it down into...
+Assistant: Great! Let's continue from there.
+"""
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+if __name__ == "__main__":
+    app.run(debug=True)
+
